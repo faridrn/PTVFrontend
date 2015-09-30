93c013bf74610a3777d180be77d137b3b5f82055
Farid Rn <faridv@gmail.com>
2015-09-30 21:33:52 +0330

    Working on search animtaions

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
-$(function() {
-    $(".header-menu .search > a").click(function(e) {
-	$(this).next().toggle(300);
-	e.preventDefault();
+$(function () {
+    // Search collapse
+    $(".header-menu .search > a").click(function (e) {
+        var $search = $(this).next();
+        $search.animate({ width: 260 , padding: 5 }, function() {
+            $search.find("input").focus();
+        });
+        $search.on('focusout', "input", function() {
+            $search.animate({width: 0, padding: '5px 0'});
+        });
+        e.preventDefault();
     });
 });
